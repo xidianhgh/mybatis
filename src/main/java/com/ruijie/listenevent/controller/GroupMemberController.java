02e14e56f24c37f892776577b1cf954c05744d9c
xidianhgh <xidianhgh@163.com>
2022-03-12 21:12:06 +0800

    增加一个类

---
+++ src/main/java/com/ruijie/listenevent/controller/GroupMemberController.java
@@ -0,0 +1,26 @@
+package com.ruijie.listenevent.controller;
+
+import com.ruijie.listenevent.entity.GroupMemberEntity;
+import com.ruijie.listenevent.service.GroupMemberService;
+import com.ruijie.listenevent.utils.DateUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class GroupMemberController {
+    @Autowired
+    GroupMemberService groupMemberService;
+
+    @PostMapping("/groups")
+    public void test(@RequestBody GroupMemberEntity req) {
+
+        String currentDate = DateUtils.getCurrentTime();
+        groupMemberService.saveGroupMemberEvent(req.getType(), req.getMember(), req.getExecutor(),
+                currentDate, req.getGroupName());
+    }
+
+}
