730b230ac6347a2e64d5a14d8d4f68eaf3725bcb
xidianhgh <xidianhgh@163.com>
2022-06-21 18:45:06 +0800

    测试@Transactional

--- src/main/java/com/ruijie/listenevent/controller/GroupController.java
+++ src/main/java/com/ruijie/listenevent/controller/GroupController.java
@@ -4,6 +4,7 @@ import com.ruijie.listenevent.entity.GroupEntity;
 import com.ruijie.listenevent.service.GroupService;
 import com.ruijie.listenevent.utils.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,16 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.Date;
 
 @RestController
+//@Transactional
 @RequestMapping("/group")
 public class GroupController {
     @Autowired
     GroupService groupService;
 
     @PostMapping("")
+    @Transactional
     public void test(@RequestBody GroupEntity req) {
         String currentDate = DateUtils.getCurrentTime();
         req.setDate(new Date());
         groupService.saveData(req);
+    }
+
+    @PostMapping("/transcation")
+    public void testTranscation(@RequestBody GroupEntity req) {
+        String currentDate = DateUtils.getCurrentTime();
+        req.setDate(new Date());
+        groupService.saveTransactional(req);
     }
 
 }
--- src/main/java/com/ruijie/listenevent/service/GroupService.java
+++ src/main/java/com/ruijie/listenevent/service/GroupService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 @Service
+//@Transactional
 public class GroupService {
     @Autowired
     GroupMapper groupMapper;
@@ -23,4 +24,18 @@ public class GroupService {
     public void createTable() {
         groupMapper.createNewTable("group_info");
     }
+
+    public void saveTransactional(GroupEntity req) {
+        createTable();
+        GroupEntity groupEntity = new GroupEntity();
+        BeanUtils.copyProperties(req, groupEntity);
+        groupMapper.insert(groupEntity);
+        GroupEntity groupEntity1 = new GroupEntity();
+        BeanUtils.copyProperties(req, groupEntity1);
+        groupEntity1.setGroupName("xxx");
+        groupMapper.insert(groupEntity1);
+        GroupEntity groupEntity2=null;
+        groupEntity2.setGroupName("a");
+    }
+
 }
